932787a6b7c294bb2403bc5245071772a9465e12
John Wang <johncwang@gmail.com>
2023-08-20 21:04:22 -0700

    enhance: add Status inflation to `IssueFromAPI()`

--- issue_api.go
+++ issue_api.go
@@ -12,6 +12,7 @@ func IssueFromAPI(iss jira.Issue) Issue {
 		return Issue{}
 	}
 	x := Issue{
+		Key:                           Simple{DisplayName: iss.Key},
 		Summary:                       strings.TrimSpace(iss.Fields.Summary),
 		Description:                   strings.TrimSpace(iss.Fields.Description),
 		TimeEstimate:                  Duration{Seconds: int64(iss.Fields.TimeEstimate)},
@@ -23,12 +24,29 @@ func IssueFromAPI(iss jira.Issue) Issue {
 		Created:                       RFC1123ZStringJiraTime(iss.Fields.Created),
 		Updated:                       RFC1123ZStringJiraTime(iss.Fields.Updated),
 	}
+	x.Type = Simple{
+		DisplayName: iss.Fields.Type.Name,
+	}
 	if r := iss.Fields.Resolution; r != nil && len(strings.TrimSpace(r.ID)) > 0 {
 		id, err := strconv.Atoi(r.ID)
 		if err != nil {
 			panic(err)
 		}
-		x.Resolution = Simple{ID: id, DisplayName: strings.TrimSpace(r.Name)}
+		x.Resolution = Simple{
+			ID:          id,
+			DisplayName: strings.TrimSpace(r.Name)}
+	}
+	if s := iss.Fields.Status; s != nil && len(strings.TrimSpace(s.ID)) > 0 {
+		id, err := strconv.Atoi(s.ID)
+		if err != nil {
+			panic(err)
+		}
+		x.Status = Status{
+			ID:          id,
+			Description: strings.TrimSpace(s.Description),
+			DisplayName: strings.TrimSpace(s.Name),
+			IconURL:     strings.TrimSpace(s.IconURL),
+		}
 	}
 	return x
 }
