b271b5375e5f5cf72bb6f544fc2ba004d2447366
John Wang <johncwang@gmail.com>
2023-08-27 03:45:44 -0700

    feat: `TimeStats`: add `TimeStats.SecondsToDays()`

--- time_stats.go
+++ time_stats.go
@@ -1,7 +1,10 @@
 package gojira
 
 import (
+	"fmt"
+
 	jira "github.com/andygrunwald/go-jira"
+	"github.com/grokify/mogo/time/timeutil"
 )
 
 type TimeStatsSets struct {
@@ -19,25 +22,40 @@ type TimeStatsSet struct {
 }
 
 type TimeStats struct {
+	TimeUnit                      string
 	WorkingHoursPerDay            float32
 	WorkingDaysPerWeek            float32
 	ItemCount                     int
-	TimeSpent                     int
-	TimeEstimate                  int
-	TimeOriginalEstimate          int
-	AggregateTimeOriginalEstimate int
-	AggregateTimeSpent            int
-	AggregateTimeEstimate         int
-	TimeRemaining                 int
-	TimeRemainingOriginal         int
-	/*
-		ItemCountByStatus  map[string]int
-		ItemCountByType    map[string]int
-		//EstimateStatsByType      map[string]EstimateStats
-		TimeOriginalEstimate     time.Duration
-		TimeOriginalEstimateDays float64
-		AggregateTimeSpent       time.Duration
-		AggregateTimeSpentDays   float64
-		//ClosedEstimateVsActual   EstimateVsActual
-	*/
+	TimeSpent                     float32
+	TimeEstimate                  float32
+	TimeOriginalEstimate          float32
+	AggregateTimeOriginalEstimate float32
+	AggregateTimeSpent            float32
+	AggregateTimeEstimate         float32
+	TimeRemaining                 float32
+	TimeRemainingOriginal         float32
+}
+
+func (ts TimeStats) SecondsToDays() (TimeStats, error) {
+	if ts.TimeUnit != timeutil.SecondString {
+		return ts, fmt.Errorf("time unit is not seconds, is (%s)", ts.TimeUnit)
+	}
+	if ts.WorkingHoursPerDay <= 0 {
+		return ts, fmt.Errorf("workingHoursPerDay must be greater than 0, is (%v)", ts.WorkingHoursPerDay)
+	}
+	whpd := ts.WorkingHoursPerDay
+	tsDays := TimeStats{
+		TimeUnit:                      timeutil.DayString,
+		WorkingHoursPerDay:            ts.WorkingHoursPerDay,
+		WorkingDaysPerWeek:            ts.WorkingDaysPerWeek,
+		ItemCount:                     ts.ItemCount,
+		TimeEstimate:                  ts.TimeEstimate / 60 / 60 / whpd,
+		TimeOriginalEstimate:          ts.TimeOriginalEstimate / 60 / 60 / whpd,
+		AggregateTimeOriginalEstimate: ts.AggregateTimeOriginalEstimate / 60 / 60 / whpd,
+		AggregateTimeSpent:            ts.AggregateTimeSpent / 60 / 60 / whpd,
+		AggregateTimeEstimate:         ts.AggregateTimeEstimate / 60 / 60 / whpd,
+		TimeRemaining:                 ts.TimeRemaining / 60 / 60 / whpd,
+		TimeRemainingOriginal:         ts.TimeRemainingOriginal / 60 / 60 / whpd,
+	}
+	return tsDays, nil
 }
