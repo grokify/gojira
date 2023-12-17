fd2b8021cb310838396108daa586c1a1932f0c63
John Wang <johncwang@gmail.com>
2023-12-17 15:29:03 -0800

    feat: `jirarest.IssuesSet{}`: add `IssueOrParent()`, `KeysParents()`, `KeysParentsPopulated()`, `KeysParentsUnpopulated()`

--- jirarest/issues_set_parents.go
+++ jirarest/issues_set_parents.go
@@ -2,9 +2,12 @@ package jirarest
 
 import (
 	"errors"
-	"strings"
+	"fmt"
 
+	jira "github.com/andygrunwald/go-jira"
+	"github.com/grokify/gojira"
 	"github.com/grokify/mogo/errors/errorsutil"
+	"github.com/grokify/mogo/fmt/fmtutil"
 	"github.com/grokify/mogo/pointer"
 	"github.com/grokify/mogo/type/stringsutil"
 )
@@ -29,14 +32,14 @@ func (is *IssuesSet) RetrieveParentsIssuesSet(client *Client) (*IssuesSet, error
 */
 
 func (is *IssuesSet) RetrieveParents(client *Client) error {
-	parIDs := is.UnknownParents()
+	parIDs := is.KeysParentsUnpopulated()
 	i := 0
 	for len(parIDs) > 0 {
 		err := is.RetrieveIssues(client, parIDs)
 		if err != nil {
 			return err
 		}
-		parIDs = is.UnknownParents()
+		parIDs = is.KeysParentsUnpopulated()
 		i++
 		if i > 10 {
 			return errors.New("exceeded max retrieve parent iterations")
@@ -46,32 +49,91 @@ func (is *IssuesSet) RetrieveParents(client *Client) error {
 }
 
 func (is *IssuesSet) RetrieveIssues(client *Client, ids []string) error {
+	if client == nil {
+		return errors.New("client cannot be nil")
+	}
 	ids = stringsutil.SliceCondenseSpace(ids, true, true)
 	if len(ids) == 0 {
 		return nil
 	}
-	jql := "key in (" + strings.Join(ids, ",") + ")"
-	iss, err := client.SearchIssues(jql)
-	if err != nil {
-		return err
+	fmt.Printf("LEN(%d)\n", len(ids))
+
+	idsSlicesMaxResults := SplitMaxLength(ids, gojira.JQLMaxResults)
+	fmtutil.PrintJSON(idsSlicesMaxResults)
+
+	for _, idsSlice := range idsSlicesMaxResults {
+		jqls := gojira.JQLStringsSimple(gojira.FieldKey, false, idsSlice, 0)
+		//fmtutil.PrintJSON(jqls)
+		//panic("Z")
+
+		for _, jql := range jqls {
+			if iss, err := client.SearchIssuesPages(jql, 0, 0, 0); err != nil {
+				return err
+			} else {
+				return is.Add(iss...)
+			}
+			/*
+				// jql := "key in (" + strings.Join(ids, ",") + ")"
+				if iss, err := client.SearchIssues(jql); err != nil {
+					return err
+				} else {
+					return is.Add(iss...)
+				}
+			*/
+		}
 	}
-	return is.Add(iss...)
+	return nil
 }
 
-func (is *IssuesSet) UnknownParents() []string {
-	parKeys := []string{}
+func (is *IssuesSet) IssueOrParent(key string) (*jira.Issue, bool) {
+	if iss, ok := is.IssuesMap[key]; ok {
+		return &iss, true
+	} else if is.Parents == nil {
+		return nil, false
+	} else if iss, ok := is.Parents.IssuesMap[key]; ok {
+		return &iss, true
+	} else {
+		return nil, false
+	}
+}
+
+func (is *IssuesSet) KeysParents() []string {
+	var parKeys []string
 	for _, iss := range is.IssuesMap {
 		im := IssueMore{Issue: pointer.Pointer(iss)}
-		parKey := im.ParentKey()
-		if parKey == "" {
-			continue
+		if parKey := im.ParentKey(); parKey != "" {
+			parKeys = append(parKeys, parKey)
 		}
-		if _, ok := is.IssuesMap[parKey]; ok {
-			continue
-		}
-		parKeys = append(parKeys, parKey)
 	}
 	return stringsutil.SliceCondenseSpace(parKeys, true, true)
+}
+
+// ParentsPopulated returns issue ids that are in the current set or current parent set.
+func (is *IssuesSet) KeysParentsPopulated() []string {
+	var parKeysPop []string
+	parKeysAll := is.KeysParents()
+	for _, parKey := range parKeysAll {
+		parIss, ok := is.IssueOrParent(parKey)
+		if ok && parIss != nil {
+			parKeysPop = append(parKeysPop, parKey)
+		}
+	}
+
+	return stringsutil.SliceCondenseSpace(parKeysPop, true, true)
+}
+
+// ParentsUnpopulated returns issue ids that are not in the current set or current parent set.
+func (is *IssuesSet) KeysParentsUnpopulated() []string {
+	var parKeysUnpop []string
+	parKeysAll := is.KeysParents()
+	for _, parKey := range parKeysAll {
+		parIss, ok := is.IssueOrParent(parKey)
+		if !ok || parIss == nil {
+			parKeysUnpop = append(parKeysUnpop, parKey)
+		}
+	}
+
+	return stringsutil.SliceCondenseSpace(parKeysUnpop, true, true)
 }
 
 // Lineage returns a slice of `IssueMeta` where the suppied key is in index 0 and the most senior
