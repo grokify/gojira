d7fc1e7e6cc72256fbde7821f3bc4b482e8a8eb1
John Wang <johncwang@gmail.com>
2023-12-17 04:31:37 -0800

    feat: `jirarest`: add Jira issue type constants

--- constants.go
+++ constants.go
@@ -3,15 +3,26 @@ package gojira
 import "time"
 
 const (
+	// Statuses: https://support.atlassian.com/jira-cloud-administration/docs/what-are-issue-statuses-priorities-and-resolutions/
 	StatusClosed            = "Closed"
+	StatusDone              = "Done"
 	StatusInProgress        = "In Progress"
 	StatusPOReview          = "PO Review"
 	StatusPendingValidation = "Pending Validation"
 	StatusReady             = "Ready"
 
-	TypeBug   = "Bug"
-	TypeSpike = "Spike"
-	TypeStory = "Story"
+	TypeIssue           = "Issue"
+	TypeIssuePlural     = "Issues"
+	TypeBug             = "Bug"
+	TypeBugPlural       = "Bugs"
+	TypeEpic            = "Epic"
+	TypeEpicPlural      = "Epics"
+	TypeSpike           = "Spike"
+	TypeSpikePlural     = "Spikes"
+	TypeStory           = "Story"
+	TypeStoryPlural     = "Stories"
+	TypeInitiative      = "Initiative"
+	TypeInitiativePlura = "Initiatives"
 
 	WorkingHoursPerDayDefault float32 = 8.0
 	WorkingDaysPerWeekDefault float32 = 5.0
