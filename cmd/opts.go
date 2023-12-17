4c7782eb2a768fb35946bfa3d6738da1df000c8d
John Wang <johncwang@gmail.com>
2023-12-17 04:32:01 -0800

    feat: `cmd`: add `NewOptions()`

--- cmd/opts.go
+++ cmd/opts.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/grokify/gojira/jirarest"
+	flags "github.com/jessevdk/go-flags"
 )
 
 type Options struct {
@@ -9,10 +10,17 @@ type Options struct {
 	Authkey         string `short:"k" long:"Goauth key" description:"Goauth credentials Key"`
 	BoardID         uint   `short:"b" long:"boardid" description:"Jira Board ID"`
 	IssueKey        string `short:"i" long:"key" description:"Jira Issue Key"`
+	JQL             string `short:"j" long:"jql" description:"Jira Query Language"`
 	Customfield     string `short:"c" long:"customfield" description:"Custom field"`
 	CustomfieldName string `short:"n" long:"customfield name" description:"Custom field name"` // 'Epic Link'
 }
 
 func (opts Options) Client() (*jirarest.Client, error) {
-	return jirarest.ClientsBasicAuthFile(opts.Authfile, opts.Authkey)
+	return jirarest.NewClientGoauthBasicAuthFile(opts.Authfile, opts.Authkey)
+}
+
+func NewOptions() (Options, error) {
+	opts := Options{}
+	_, err := flags.Parse(&opts)
+	return opts, err
 }
