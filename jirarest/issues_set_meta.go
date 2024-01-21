196472442e4241bdea68c799e66af652c5f0943e
John Wang <johncwang@gmail.com>
2024-01-21 01:31:10 -0800

    feat: `jirarest`: add `IssuesSet.Types()`, `IssuesSet.KeysForTypes()`

---
+++ jirarest/issues_set_meta.go
@@ -0,0 +1,71 @@
+package jirarest
+
+import (
+	"github.com/grokify/mogo/type/maputil"
+	"github.com/grokify/mogo/type/stringsutil"
+)
+
+func (is *IssuesSet) Types(inclBase, inclParents bool) []string {
+	var types []string
+	if inclBase {
+		t := is.types()
+		types = append(types, t...)
+	}
+	if inclParents && is.Parents != nil {
+		t := is.Parents.Types(inclParents, inclParents)
+		// t := is.Parents.types()
+		types = append(types, t...)
+	}
+	return stringsutil.SliceCondenseSpace(types, true, true)
+}
+
+func (is *IssuesSet) types() []string {
+	types := map[string]int{}
+	for _, iss := range is.IssuesMap {
+		iss := iss
+		im := IssueMore{Issue: &iss}
+		types[im.Type()]++
+	}
+	return maputil.Keys(types)
+}
+
+func (is *IssuesSet) KeysForTypes(types []string, inclBase, inclParents bool) []string {
+	if len(types) == 0 ||
+		(!inclBase && !inclParents) {
+		return []string{}
+	}
+	var keys []string
+	if inclBase {
+		k := is.keysForTypes(types)
+		keys = append(keys, k...)
+	}
+	if inclParents && is.Parents != nil {
+		k := is.Parents.KeysForTypes(types, inclParents, inclParents)
+		// k := is.Parents.keysForTypes(types)
+		keys = append(keys, k...)
+		if inclBase {
+			keys = stringsutil.SliceCondenseSpace(keys, true, true)
+		}
+	}
+	return keys
+}
+
+func (is *IssuesSet) keysForTypes(types []string) []string {
+	if len(types) == 0 {
+		return []string{}
+	}
+	typeMap := map[string]int{}
+	for _, t := range types {
+		typeMap[t]++
+	}
+	var keys []string
+	for _, iss := range is.IssuesMap {
+		iss := iss
+		im := IssueMore{Issue: &iss}
+		t := im.Type()
+		if _, ok := typeMap[t]; ok {
+			keys = append(keys, im.Key())
+		}
+	}
+	return stringsutil.SliceCondenseSpace(keys, true, true)
+}
