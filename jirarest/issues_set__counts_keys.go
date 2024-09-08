06c607041d24a8b5c486c7e4f0da93820e682ac1
John Wang <johncwang@gmail.com>
2024-09-08 11:53:25 -0700

    feat: `jirarest`: add `IssuesSet.KeysByField()`, `SliceMore{}`

---
+++ jirarest/issues_set__counts_keys.go
@@ -0,0 +1,36 @@
+package jirarest
+
+import "sort"
+
+func (set *IssuesSet) KeysByField(fieldLabel string) map[string]SliceMore {
+	out := map[string]SliceMore{}
+	for _, iss := range set.IssuesMap {
+		im := NewIssueMore(&iss)
+		if v, ok := im.Value(fieldLabel); ok {
+			if _, ok := out[v]; !ok {
+				out[v] = SliceMore{Values: []string{}}
+			}
+			if sm, ok := out[v]; !ok {
+				panic("vals out found")
+			} else {
+				sm.Values = append(sm.Values, im.Key())
+				out[v] = sm
+			}
+		}
+	}
+	for k, sm := range out {
+		sm.Inflate()
+		out[k] = sm
+	}
+	return out
+}
+
+type SliceMore struct {
+	Count  int      `json:"count"`
+	Values []string `json:"values"`
+}
+
+func (sm *SliceMore) Inflate() {
+	sort.Strings(sm.Values)
+	sm.Count = len(sm.Values)
+}
