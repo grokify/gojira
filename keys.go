9962df93563aa784f1b2ffac9e6d03d5fc01d6d0
John Wang <johncwang@gmail.com>
2024-09-09 03:26:33 -0700

    refactor: `gojira`: move `ParseKeys()` from `jiraweb`

--- keys.go
+++ keys.go
@@ -1,16 +1,20 @@
 package gojira
 
-/*
-func BuildIssueKeyURL(baseURL, key string) string {
-	key = strings.TrimSpace(key)
-	if urlutil.IsHTTP(key, true, true) {
-		return key
-	}
-	return fmt.Sprintf(JiraIssueKeyURL, baseURL, key)
-}
+import (
+	"regexp"
 
-func BuildIssueKeyURLMarkdown(baseURL, key string) string {
-	keyURL := BuildIssueKeyURL(baseURL, key)
-	return markdown.Linkify(keyURL, key)
+	"github.com/grokify/mogo/type/stringsutil"
+)
+
+var rxJiraTicket = regexp.MustCompile(`([A-Z]+\-[0-9]+)`)
+
+func ParseKeys(s string, unique, asc bool) []string {
+	var keys []string
+	m := rxJiraTicket.FindAllStringSubmatch(s, -1)
+	for _, n := range m {
+		if len(n) > 1 {
+			keys = append(keys, n[1])
+		}
+	}
+	return stringsutil.SliceCondenseSpace(keys, unique, asc)
 }
-*/
