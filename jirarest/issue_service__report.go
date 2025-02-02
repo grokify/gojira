8527c9a51e66f58751e4f641e645e0a82ad02811
John Wang <johncwang@gmail.com>
2025-02-02 15:02:41 -0800

    feat: `jirarest`: add `IssueService.JQLsReportMarkdownLines()`

---
+++ jirarest/issue_service__report.go
@@ -0,0 +1,24 @@
+package jirarest
+
+import (
+	"strings"
+
+	"github.com/grokify/gojira"
+	"github.com/grokify/mogo/net/urlutil"
+)
+
+// JQLsReportMarkdownLines provides Markdownlines for a set of JQLs, including querying the number
+// of results for each JQL via the Jira API.
+func (svc *IssueService) JQLsReportMarkdownLines(headerPrefix string, jqls []gojira.JQL, opts gojira.JQLsReportMarkdownOpts) ([]string, error) {
+	jqls, err := svc.JQLsAddMetadata(jqls)
+	if err != nil {
+		return []string{}, err
+	}
+	issuesWebURL := ""
+	if svc.Client != nil && svc.Client.Config != nil {
+		if svrURL := strings.TrimSpace(svc.Client.Config.ServerURL); svrURL != "" {
+			issuesWebURL = urlutil.JoinAbsolute(svrURL, "issues/?")
+		}
+	}
+	return gojira.JQLsReportMarkdownLines(issuesWebURL, headerPrefix, jqls, opts)
+}
